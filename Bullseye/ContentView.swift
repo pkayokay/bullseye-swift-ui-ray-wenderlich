5d7ffda0252fa5df33362e7443c974dfa72d463e
Paul Kim <paulk@hey.com>
2021-04-16 07:15:39 -0400

    Update view components styles with modifiers

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,13 +11,26 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-            Text("89")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4)
+                .font(.footnote)
+            
+            Text("89") 
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
             HStack {
                 Text("1")
+                    .bold()
                 Slider(value: .constant(50), in: 1.0...100.0)
                 Text("100")
+                    .bold()
             }
-            Button(action: {}) {
+            Button(action: {
+                print("dasdasd")
+            }) {
                 Text("Hit Me!")
             }
         }
